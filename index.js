2ebb3d625f5075a5d688e2eb2287b94bd370b08c
Basile Bruneau <basilebruneau@gmail.com>
2017-01-31 18:27:41 +0100

    Add route find-music-video

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const alltomp3 = require('alltomp3');
 const request = require('request-promise');
-const cors = require('cors')
-var things = [];
+const cors = require('cors');
 let appApi = express();
 const portApi = 8080;
 
@@ -17,6 +16,22 @@ appApi.get('/v1/guess-track/:q', function (req, res) {
   }).catch(e => {
     console.log(e);
     res.status(404).send({error: "No track found"});
+  });
+});
+
+appApi.get('/v1/find-music-video/', function (req, res) {
+  if (!req.query.title || !req.query.artist) {
+    return res.status(400).send({error: "title or artist missing"});
+  }
+  alltomp3.findVideoForSong({title: req.query.title, artistName: req.query.artist, duration: req.query.duration}).then(videos => {
+    videos.forEach(v => {
+      delete v.realLike;
+      delete v.videoScore;
+    });
+    res.send(videos);
+  }).catch(e => {
+    console.log(e);
+    res.status(404).send({error: "No videos found"});
   });
 });
 
